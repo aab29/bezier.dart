22a9af9ac2581a2b99822e4e44106755d979a324
Aaron Barrett <aaron@aaronbarrett.com>
2019-01-29 14:07:59 -0800

    Adding `bezier_tools` to the `testing_tools` so that we don't have to import it each time we write a test of one of the tools.

--- test/testing_tools/testing_tools.dart
+++ test/testing_tools/testing_tools.dart
@@ -3,6 +3,7 @@ export "package:test/test.dart";
 export "package:vector_math/vector_math.dart";
 
 export "package:bezier/bezier.dart";
+export "package:bezier/src/bezier_tools.dart";
 
 export "matchers/close_to_double.dart";
 export "matchers/close_to_vector.dart";
